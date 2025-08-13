fceaf7bd30fc3df260ccd226aa12844e160a2c17
zidariu-sabin <zidariu19@gmail.com>
2025-08-13 17:49:56 +0300

    dfs, learning balancing trees and tree operations

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,46 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+import Queue from "./Queue";
 
+function compareBF(a: BinaryNode<number> | null, b: BinaryNode<number> | null){
+    let queue = new Queue<BinaryNode<number> | null>();
+    queue.enqueue(a)
+    queue.enqueue(b)
+    while(queue.length){
+        let firstQueueEl = queue.deque()
+        let secondQueueEl = queue.deque()
+
+        if(firstQueueEl === null && secondQueueEl === null ){
+            continue;
+        }
+        if(firstQueueEl?.value !== secondQueueEl?.value){
+            return false
+        }
+
+        queue.enqueue(firstQueueEl?.left ?? null)
+        queue.enqueue(secondQueueEl?.left ?? null)
+
+        
+        queue.enqueue(firstQueueEl?.right ?? null)
+        queue.enqueue(secondQueueEl?.right ?? null)
+    }
+
+    return true
+}
+
+function compareDF(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if (a === null && b === null){
+        return true
+    }
+
+    if( a === null || b === null ){
+        return false
+    }
+
+    if( a.value !== b.value){
+        return false
+    }
+
+    return compareDF(a.left, b.left) && compareDF(a.right, b.right)
+}
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    return compareDF(a, b)
 }
--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,17 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+export default function dfs(head: BinaryNode<number> | null, needle: number): boolean {
+    if(head === null){
+        return false
+    }
 
-}
+    if(head.value === needle){
+        return true
+    }
+
+    if(head.value < needle){
+        return dfs(head.right, needle)
+    }else{
+        return dfs(head.left, needle)
+    }
+
+
+}   
