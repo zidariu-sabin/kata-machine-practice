1f4f996656f549654e6d54dcc30e6ebd974161a1
zidariu-sabin <zidariu19@gmail.com>
2025-10-05 19:34:11 +0300

    trie still wip

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -15,61 +15,94 @@ export default class Trie {
             children: Array<Node >(26)
         }
     }
-//@ts-ignore
+
     insert(item: string): void {
         let curr: Node  = this.head
         while(item.length){
             let char = item[0]
             const idx = char.charCodeAt(0) - Trie.firstCharacterCode.charCodeAt(0);
-            if(curr.children[idx] === null){
+            if((curr.children[idx]?.val ===undefined ? null : curr.children[idx]?.val) === null){
                 curr.children[idx] = {
                     isWord: item.length == 1,
                     val: char,
                     children: Array<Node >(26)
                 }
-            }else{
+            }
+            if(item.length == 1 && curr.children[idx]){
+                curr.children[idx].isWord = true
+            }
             item = item.slice(1)
             curr = curr.children[idx]
+        }
+    }
+
+    delete(item: string): void {
+        let curr = this.head
+        let idx = 0
+        for(let i=0; i<item.length; i++){
+            idx = item.charCodeAt(i) - Trie.firstCharacterCode.charCodeAt(0);
+            if(curr.children[idx]){
+                curr = curr.children[idx]
             }
         }
+        curr.isWord = false
     }
-    delete(item: string): void {
 
-}
     find(partial: string): string[] {
-    let curr = this.head
-    for(let i=0; i<partial.length; i++){
-        const idx = partial.charCodeAt(i) - Trie.firstCharacterCode.charCodeAt(0);
-        if(curr.children[idx]){
-            curr = curr.children[idx]
+        let curr = this.head
+        for(let i=0; i<partial.length; i++){
+            const idx = partial.charCodeAt(i) - Trie.firstCharacterCode.charCodeAt(0);
+            if(curr.children[idx]){
+                curr = curr.children[idx]
+            }
         }
-    }
-    let strings: string[] = [];
-    // while(!curr.children.every((node) => node === null)){
-        for(let i=0; i<26; i++){
-        if (curr?.children[i] !== null){
+        let strings: string[] = [];
+        // while(!curr.children.every((node) => node === null)){
+        if(curr.isWord){
             strings.push(partial)
+        }
+        if(curr.children.every((node) => node?.val === undefined)){
+            return  strings
+        }
+        strings.push(partial)
+        this.findCompletion(curr,strings.length-1,strings)
+            // for(let i=0; i<26; i++){
+            //     if (curr?.children[i]?.val !== undefined){
+                    
+            //     }
+            // }
+
+        // }
+
+        return strings.slice(1)
+    }
+
+    private findCompletion(curr:Node,idx:number,strings: string[]){
+        if(curr?.val === undefined){
+            return  
+        }
+        
+
+        //if a word is found and there are still children available it means there are more words
+        if(curr.children.every((node) => node?.val === undefined)){
+            return  
+        }
+
+        for(let i=0; i<26; i++){
+            //for each next letter found we create a branch
+            if (curr?.children[i]?.val !== undefined){
+                curr = curr.children[i]
+                if(curr.val){
+                    strings[idx]=strings[idx].concat(curr.val)   
+                }
+                //if current letter is the end of a word we must split 
+                // between the current word and the folowing ones
+                if(curr.isWord){
+                    return
+                }
+                strings.push(strings[idx])
                 this.findCompletion(curr,strings.length-1,strings)
             }
         }
-
-    // }
-    return strings
-}
-
-private findCompletion(curr:Node,idx:number,strings: string[]):string []{
-    if(curr.val){
-        strings[idx].concat(curr.val)   
-    } 
-    if(curr.children.every((node) => node.val === null)){
-        return strings  
     }
-    for(let i=0; i<26; i++){
-        if (curr?.children[i] !== null){
-            strings.push(strings[idx])
-            this.findCompletion(curr,strings.length-1,strings)
-        }
-    }
-    return strings
-}
 }
