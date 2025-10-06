61e2374b34807174aa25d51f74bde28ba1e49a0e
zidariu-sabin <zidariu19@gmail.com>
2025-10-06 16:38:12 +0300

    finished trie implementation with cleaner code

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -6,7 +6,16 @@ type Node = {
 export default class Trie {
     private head: Node
 
-    private static firstCharacterCode = 'a'
+
+    private static readonly firstCharacterCode = 'a'.charCodeAt(0)
+
+    private static charIndex(ch: string){
+        const idx = ch.charCodeAt(0) - Trie.firstCharacterCode
+        if (idx < 0 || idx >= 26) {
+            throw new RangeError(`charIndex: character out of range: ${ch}`)
+        }
+        return idx
+    }
 
     constructor() {
         this.head = {
@@ -20,7 +29,7 @@ export default class Trie {
         let curr: Node  = this.head
         while(item.length){
             let char = item[0]
-            const idx = char.charCodeAt(0) - Trie.firstCharacterCode.charCodeAt(0);
+            const idx = Trie.charIndex(char)
             if((curr.children[idx]?.val ===undefined ? null : curr.children[idx]?.val) === null){
                 curr.children[idx] = {
                     isWord: item.length == 1,
@@ -40,7 +49,7 @@ export default class Trie {
         let curr = this.head
         let idx = 0
         for(let i=0; i<item.length; i++){
-            idx = item.charCodeAt(i) - Trie.firstCharacterCode.charCodeAt(0);
+            idx = Trie.charIndex(item[i]);
             if(curr.children[idx]){
                 curr = curr.children[idx]
             }
@@ -48,17 +57,24 @@ export default class Trie {
         curr.isWord = false
     }
 
-    find(partial: string): string[] {
+    private parseWord(item: string): Node {
         let curr = this.head
-        for(let i=0; i<partial.length; i++){
-            const idx = partial.charCodeAt(i) - Trie.firstCharacterCode.charCodeAt(0);
+        for(let i=0; i<item.length; i++){
+            const idx = Trie.charIndex(item[i]);
             if(curr.children[idx]){
                 curr = curr.children[idx]
             }
         }
+        return curr
+    }
+
+    find(partial: string): string[] {
+        let curr = this.parseWord(partial)
         let strings: string[] = [];
-        // while(!curr.children.every((node) => node === null)){
+        let partialIsWord = false
+        //if a word is given and we want to search also the following
         if(curr.isWord){
+            partialIsWord  = true;
             strings.push(partial)
         }
         if(curr.children.every((node) => node?.val === undefined)){
@@ -66,43 +82,45 @@ export default class Trie {
         }
         strings.push(partial)
         this.findCompletion(curr,strings.length-1,strings)
-            // for(let i=0; i<26; i++){
-            //     if (curr?.children[i]?.val !== undefined){
-                    
-            //     }
-            // }
 
-        // }
-
-        return strings.slice(1)
+        if(partialIsWord){
+            return strings.slice(1)
+        }
+        return strings
     }
 
     private findCompletion(curr:Node,idx:number,strings: string[]){
         if(curr?.val === undefined){
             return  
         }
-        
-
         //if a word is found and there are still children available it means there are more words
         if(curr.children.every((node) => node?.val === undefined)){
             return  
         }
 
-        for(let i=0; i<26; i++){
-            //for each next letter found we create a branch
-            if (curr?.children[i]?.val !== undefined){
-                curr = curr.children[i]
+        if(curr.isWord){
+            strings.push(strings[idx])
+            curr.children.forEach((child) =>{
+                //for each next letter found we create a branch
+                curr = child
                 if(curr.val){
-                    strings[idx]=strings[idx].concat(curr.val)   
+                    strings[strings.length-1]=strings[idx].concat(curr.val)   
                 }
                 //if current letter is the end of a word we must split 
                 // between the current word and the folowing ones
-                if(curr.isWord){
-                    return
-                }
-                strings.push(strings[idx])
                 this.findCompletion(curr,strings.length-1,strings)
-            }
+            })
+            
+        }else{
+            curr.children.forEach((child) =>{
+                //for each next letter found we create a branch
+                curr = child
+                if(curr.val){
+                    strings[idx]=strings[idx].concat(curr.val)   
+                }
+                 this.findCompletion(curr,idx,strings)
+            })
+           
         }
     }
 }
